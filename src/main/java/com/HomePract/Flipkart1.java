09612a2df3706388b6aca59d83eb112ba3515170
bimlajha <mukeshjha.ist@gmail.com>
2020-01-16 16:35:21 +0530

    16jan file uploaded

---
+++ src/main/java/com/HomePract/Flipkart1.java
@@ -0,0 +1,20 @@
+package com.HomePract;
+
+import org.testng.annotations.Test;
+
+public class Flipkart1 {
+	
+	@Test
+	public void loginest()
+	{
+		System.out.println("hello");
+		
+	}
+	@Test
+	public void loginest1()
+	{
+		System.out.println("hello1");
+		
+	}
+
+}
